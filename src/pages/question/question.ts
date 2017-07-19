bfeb764b47288b77a1b60dda1505da3cc15aa1d0
valt <valt@valts-MBP.T-mobile.com>
2017-07-19 11:54:34 -0400

    refactor for firebase & removed socket.io

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -1,28 +1,42 @@
 import { Component } from '@angular/core';
-import * as io from "socket.io-client";
+import { User, Auth } from '@ionic/cloud-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// import * as io from "socket.io-client";
 
 @Component({
   selector: 'page-question',
   templateUrl: 'question.html'
 })
 export class QuestionPage {
+  // socket = io(process.env.SOCKETIO || 'http://localhost:8080') ;
+
   msg = 'Type here..';
-  messages = [];
-  categories = ['Math', 'Science', 'Art', 'History'];
+  categories = ['Math'];
   currentCategory = this.categories[0];
-  socket = io(process.env.SOCKETIO || 'http://localhost:8080') ;
-  bestAnswerPicked = false;
-  
-  constructor(){
+  messageType = 'all';
 
-    this.socket.on('message', (data) => {
-      this.messages.push(data);
+  //auth
+  id = this.user.id;
+  img = this.user.details.image;
+
+  //firebase
+  questions: FirebaseListObservable<any[]>;
+
+  constructor(
+    public user: User,
+    public auth: Auth,
+    public afAuth: AngularFireAuth,
+    public af: AngularFireDatabase
+  ){
+
+    this.fetchQuestions('all');
+
+    this.questions.forEach((question)=>{
+      console.log(question);
     })
-  }
 
-  toggleBestAnswerPicked() {
-    this.bestAnswerPicked = !this.bestAnswerPicked;
-    console.log(this.bestAnswerPicked);
   }
 
   handleMsg(){
@@ -31,30 +45,78 @@ export class QuestionPage {
     }
   }
 
-  getColor(message) {
-    return message.bestAnswer ? 'green' : 'black';
+  getColor(question) {
+    return question.closed ? 'green' : 'black';
   }
 
   onSubmitMsg(){
-    this.messages.push({
-      message: this.msg,
+
+    this.sendQuestion({
+      user_id: this.id,
+      likes: 0,
+      dislikes: 0,
+      image: this.img,
+      question: this.msg,
       category: this.currentCategory,
-      username: 'Peter',
-      likes: 5,
-      img: 'http://static.srcdn.com/wp-content/uploads/Neo-stops-bullets-in-the-Matrix.jpg',
-      bestAnswer: false      
+      open_date:  Date.now(),
+      closed_date: '',
+      closed: false,
+      userid_closed: this.id + false
     });
-    this.socket.emit('message', this.messages[this.messages.length-1]);
-    this.msg = ''
+
+    this.msg = '';
+
+  }
+  
+  sendQuestion(question) {
+    this.questions.push( question );
   }
 
-  toggleAcceptAsBestAnswer(message) {
-    message.bestAnswer = !message.bestAnswer;
-    console.log('Best answer set to: ', message.bestAnswer);
+  sendQuestionUpdate(question, status){
+
+    if(status === 'likes') {
+      this.questions.update(question.$key, {likes: question.likes+1});
+    } else if (status === 'dislikes') {
+      this.questions.update(question.$key, {dislikes: question.dislikes+1});
+    } else if(status === 'closed') {
+        this.questions.update(question.$key, {closed: !question.closed});
+        this.questions.update(question.$key, {userid_closed: this.id + !question.closed});
+        this.questions.update(question.$key, {date_closed: Date.now()});
+    }
+      
   }
 
-  handleLike(message) {
-    message.likes++;
-  }
+  sendQuestionDelete(id) {
 
+    this.questions.remove(id);
+
+  }
+  
+  fetchQuestions(messageType) {
+    if(messageType === 'all') {
+      this.questions = this.af.list('/userQuestions',  {
+        query: {
+          limitToLast: 50,
+          orderByChild: 'user_id',
+          equalTo: this.id
+        }
+      });
+    } else if (messageType === 'open') {
+      this.questions = this.af.list('/userQuestions',  {
+        query: {
+          limitToLast: 50,
+          orderByChild: 'userid_closed',
+          equalTo: this.id + false
+        }
+      });
+    } else if (messageType === 'closed') {
+      this.questions = this.af.list('/userQuestions',  {
+        query: {
+          limitToLast: 50,
+          orderByChild: 'userid_closed',
+          equalTo: this.id + true
+        }
+      });
+    }
+  }
 }
