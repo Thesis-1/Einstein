6a631d2611613f42b72604745f9fb9d7335077f9
Himanshu Pant <himanshu.pn3@gmail.com>
2017-07-19 07:31:23 -0700

    setup fake api and small changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { QuestionPage } from '../question/question';
 import { User, Auth } from '@ionic/cloud-angular';
+import { App } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -12,9 +13,14 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     public user: User,
-    public auth: Auth
+    public auth: Auth,
+    public app: App
   ) {
 
+  }
+  ionViewDidLoad() {
+    this.app.setTitle('Questions');
+    // this.updateQuestionStream();
   }
 
   onGoToQuestion() {
