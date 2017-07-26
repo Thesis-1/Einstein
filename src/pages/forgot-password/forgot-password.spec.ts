371cae1e96f75dd9d485a1e9f8de5304d653bd48
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-07-26 19:29:34 -0400

    forgot password- removed old unused variables which were used for ionic auth.  Wrote tests for forgot password page (all pass).

---
+++ src/pages/forgot-password/forgot-password.spec.ts
@@ -0,0 +1,101 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { DebugElement } from '@angular/core'
+import { Http } from '@angular/http'
+import { IonicModule, Platform} from 'ionic-angular/index'
+import { App, ToastController, NavController, NavParams } from 'ionic-angular'
+
+//Mocks from mock file
+import { PlatformMock, UserMock, AuthMock, HttpMock } from '../../../test-config/mocks-ionic'
+import { AngularFireAuth } from 'angularfire2/auth';
+
+
+
+import { ForgotPasswordPage } from './forgot-password'
+
+
+
+
+// Moving FireBase Auth stuff in component to the provider(recomended) should make the tests pass -or- create mocks for firebase services here to make the tests pass
+
+class AngularFireAuthMock {
+    signInWithEmailAndPassword (email, password) {
+      return;
+    }
+}
+
+
+//Mocks for nav stuff
+class NavControllerMock {
+  push(s) {
+    return;
+  }
+
+  setRoot(s) {
+    return;
+  }
+}
+
+class NavParamsMock {
+
+}
+
+class ToastControllerMock {
+  create(obj) {
+    return;
+  }
+}
+
+
+describe('ForgotPasswordPage', () => {
+    let comp: ForgotPasswordPage
+    let fixture: ComponentFixture<ForgotPasswordPage>
+    let de: DebugElement
+    let el: HTMLElement
+
+    beforeEach(async (() => {
+        TestBed.configureTestingModule({
+            declarations: [ForgotPasswordPage],
+            imports: [
+                IonicModule.forRoot(ForgotPasswordPage)
+            ],
+            providers: [
+                { provide: AngularFireAuth, useClass: AngularFireAuthMock },
+                { provide: Http, useClass: HttpMock },
+                { provide: Platform, useClass: PlatformMock},
+                { provide: NavController, useClass: NavControllerMock},
+                { provide: NavParams, useClass: NavParamsMock},
+                { provide: ToastController, useClass: ToastControllerMock}
+            ]
+        })
+    }))
+
+    beforeEach( ()=> {
+        fixture = TestBed.createComponent(ForgotPasswordPage)
+        comp = fixture.componentInstance
+    })
+
+    it('should be sane', () => {
+      expect(true).toEqual(true);
+    });
+
+    it('should create component', () => {
+        expect(comp).toBeDefined();
+    });
+
+    it('sendReset should exist', () => {
+      expect(comp.sendReset).toBeDefined();
+    });
+
+    it('sendReset should be a function', () => {
+      expect(comp.sendReset).toEqual(jasmine.any(Function));
+    });
+
+    it('emailAddress should exist', () => {
+      expect(comp.emailAddress).toBeDefined();
+    });
+
+    it('emailAddress should initialize to an empty string', () => {
+      expect(comp.emailAddress).toEqual('');
+    });
+})
