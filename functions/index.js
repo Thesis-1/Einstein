de9f96647f6e38421fc0de8f77d703724785b024
Justine Wang <justinelhwang@gmail.com>
2017-07-29 12:37:13 -0700

    Set up translate cloud function and demo in user profile

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+const request = require('request-promise');
+const _ = require('lodash');
+
 const functions = require('firebase-functions');
 // add firebase-admin module and initialize the Firebase app using it
 const admin = require('firebase-admin');
@@ -23,6 +26,42 @@ admin.initializeApp(functions.config().firebase);
 
 // Sample cloud function. Any time our Firebase instance receives an HTTP
 // request, Firebase will send "Hello from Firebase!" in the response.
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+const LANGUAGES = ['es', 'fr', 'zh-CN'];
+
+exports.translate = function.database.ref('/translations/{translationId}').onWrite(event => {
+  const snapshot = event.data;
+  const promises = [];
+
+  _.each(LANGUAGES, (lang) => {
+    console.log(lang);
+    promises.push(createTranslationPromise(lang, snapshot));
+  });
+  return Promise.all(promises);
 });
+
+// URL to the Google Translate API
+function createTranslateUrl(lang, text) {
+  return `https://www.googleapis.com/language/translate/v2?key=${functions.config().firebase.apiKey}&source=en&target=${lang}&q=${text}`;
+}
+
+function createTranslationPromise(lang, snapshot) {
+  const key = snapshot.key;
+  const text = snapshot.val().english;
+  let translation = {};
+
+  return request(createTranslateUrl(lang, text), {resolveWithFullResponse: true}).then(
+    response => {
+      if (response.statusCode === 200) {
+        const resData = JSON.parse(response.body).data;
+        translation[lang] = resData.translations[0].translatedText;
+        return admin.database().ref(`/translations/${key}`).update(translation);
+      } else {
+        throw response.body;
+      }
+    });
+}
