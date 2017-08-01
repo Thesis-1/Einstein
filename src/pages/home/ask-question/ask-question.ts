fd4951b278e926ce291422098df9935399643341
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-08-01 19:42:42 -0400

    added FAB to ask-question and answers pages so users can post images w/ their questions and answers using the native camera.

--- src/pages/home/ask-question/ask-question.ts
+++ src/pages/home/ask-question/ask-question.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
 import { ModalController, ViewController, NavController } from 'ionic-angular'
+import { Camera } from '@ionic-native/camera';
 
 import { PreviewQuestionPage } from './preview-question/preview-question'
 import { AskedQuestionPage } from './asked-question/asked-question'
 import { StreamData } from '../../../providers/questions-stream'
+import { UtilityHelpers } from '../../../providers/utility-helpers'
 
 
 @Component({
@@ -12,9 +14,18 @@ import { StreamData } from '../../../providers/questions-stream'
 })
 
 export class AskQuestionPage  {
-    constructor (private modalCtrl: ModalController, private viewCtrl: ViewController, private navCtrl: NavController, private service: StreamData) { }
-    
+    constructor (
+      public utils: UtilityHelpers,
+      private modalCtrl: ModalController,
+      private viewCtrl: ViewController,
+      private navCtrl: NavController,
+      private service: StreamData,
+      private camera: Camera
+    )
+    { }
+
     hideTip = false
+    questionImageURL = '';
     selectedTopic = "Algebra"
     topics = [
         'Algebra',
@@ -51,6 +62,7 @@ export class AskQuestionPage  {
             createdOn: Date.now(),
             userName: this.currentUser.displayName,
             userPhotoURL: this.currentUser.photoURL,
+            imageURL: this.questionImageURL,
             userId: this.currentUser.uid,
             userClosed: this.currentUser.uid+false
         }
@@ -77,8 +89,24 @@ export class AskQuestionPage  {
         let previewModal = this.modalCtrl.create(PreviewQuestionPage, question);
         previewModal.present();
     }
- 
+
     onClickClose () {
         this.viewCtrl.dismiss()
+    }
+
+    onImageClick() {
+      this.camera.getPicture({
+        quality: 75,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        targetHeight: 240,
+        targetWidth: 240
+      }).then( (imageData) => {
+      //  let dataURL = `data:image/jpeg;base64,${imageData}`; //use for DATA_URL type
+        this.questionImageURL = imageData;
+        this.utils.popToast('Image Uploaded.  You may ask or preview your question.');
+      }, (err) => {
+        this.utils.popToast('Camera is a native feature.  Cannot use in web.')
+      });
     }
 }
